7b18c4ae31959c409ed276f6bdb7c6fe1c35df36
Alexander Ross <git.119x@bleen.net>
2010-06-15 18:31:01 +0000

    #828308 by bleen18: Fixed Provide a jump_src variable to noscript-tag.tpl.

--- dart-noscript-tag.tpl.php
+++ dart-noscript-tag.tpl.php
@@ -6,12 +6,13 @@
  * Display the js call to display a DART ad tag.
  *
  * Variables available:
- * - $tag: The full tag object or NULL. If it's NULL, all other 
- *         vars listed below will be NULL as well.
- * - $src: The src path for a noscript ad call.
+ * - $tag:      The full tag object or NULL. If it's NULL, all other 
+ *              vars listed below will be NULL as well.
+ * - $src:      The src path for a noscript ad call.
+ * - $jump_src: The src path for the <a> tag within a noscript ad call.
  *
  * @see template_preprocess_dart_noscript_tag()
  */
 ?>
   
-<noscript><a href="<?php print $src; ?>"><img src="<?php print $src; ?>" alt="" /></a></noscript>
+<noscript><a href="<?php print $jump_src; ?>"><img src="<?php print $src; ?>" alt="" /></a></noscript>
