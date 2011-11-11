afe5ed053478f6fa45561bda39d1c2e84706c8cf
bleen <git.119x@bleen.net>
2011-11-10 20:45:17 -0500

    minor js fixes

--- dart.js
+++ dart.js
@@ -19,9 +19,9 @@ Drupal.DART.tag = function(tag) {
   tag = typeof(tag) == 'string' ? eval('(' + tag + ')') : tag;
 
   var tagname = tag.settings.options.method == 'adj' ? 'script' : 'iframe';
-  var options = tag.settings.options.method == 'adj' ? 'type="text/javascript"' : 'frameborder="0" scrolling="no" width="' + tag.sz.split("x")[0] + '" height="' + tag.sz.split("x")[1] + '"';;
+  var options = tag.settings.options.method == 'adj' ? 'type="text/javascript"' : 'frameborder="0" scrolling="no" width="' + tag.sz.split("x")[0] + '" height="' + tag.sz.split("x")[1] + '"';
 
-  ad  = '<' + tagname + ' ' + options + ' src="';
+  ad = '<' + tagname + ' ' + options + ' src="';
   ad += dart_url + "/";
   ad += tag.network_id != '' ? tag.network_id + "/" : "";
   ad += tag.settings.options.method + "/";
@@ -40,7 +40,7 @@ Drupal.DART.tag = function(tag) {
   // console.log(tag);
 
   return ad;
-}
+};
 
 /**
  * Format a key|val pair into a dart tag key|val pair.
@@ -50,7 +50,7 @@ Drupal.DART.keyVal = function(key, val, useEval) {
   kvp += useEval ? eval(val) : val;
   kvp += key == "ord" ? "?" : ";";
   return(kvp);
-}
+};
 
 /**
  * Loop through an object and create kay|val pairs.
@@ -73,7 +73,7 @@ Drupal.DART.keyVals = function(vals) {
     }
   }
   return ad;
-}
+};
 
 /**
  * If there are tags in the loadLastTags, then load them where they belong.
